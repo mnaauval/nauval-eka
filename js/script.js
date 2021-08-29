6248e42aaf4f65bee1ee0086fcbc2b8d948b8d52
mnaauval <mnaauval20@gmail.com>
2021-08-29 14:17:48 +0700

    finish web page

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//  Copy to clipboard
+function copyToClipboard(elementId) {
+  // Create an auxiliary hidden input
+  var aux = document.createElement("input");
+
+  // Get the text from the element passed into the input
+  aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+
+  // Append the aux input to the body
+  document.body.appendChild(aux);
+
+  // Highlight the content
+  aux.select();
+
+  // Execute the copy command
+  document.execCommand("copy");
+
+  // Remove the input from the body
+  document.body.removeChild(aux);
+
+  //   alert("Text copied");
+  //   document.getElementById("inner-text").innerHTML = "Copied";
+}
